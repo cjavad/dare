af298ff050af73528ebac3bfa6df1e8d1bf277b5
Hjalte Nannestad <hjalte.nannestad@protonmail.com>
2022-09-14 00:57:16 +0200

    Implement solve subcommand in CLI

--- dare-cli/src/main.rs
+++ dare-cli/src/main.rs
@@ -8,7 +8,7 @@ use clipboard::ClipboardContext;
 #[cfg(target_os = "linux")]
 use clipboard_ext::x11_fork::ClipboardContext;
 
-use dare::{TableauBuilder, TableauWriter};
+use dare::{Solutions, TableauWriter};
 
 #[derive(Debug)]
 enum OutputFormat {
@@ -33,9 +33,25 @@ struct Tableau {
     /// Currently only latex is supported.
     format: OutputFormat,
 
+    /// Solve the given tableau when it evaluates to false.
+    #[clap(short = 'f', long = "false")]
+    expect_false: bool,
+
     /// Copy the output to the clipboard.
     #[clap(short, long)]
     clip_board: bool,
+
+    /// The logical expression to evaluate.
+    ///
+    /// If not provided, the expression will be read from stdin.
+    source: Option<String>,
+}
+
+#[derive(Parser, Debug)]
+struct Solve {
+    /// Solve the given tableau when it evaluates to false.
+    #[clap(short = 'f', long = "false")]
+    expect_false: bool,
 
     /// The logical expression to evaluate.
     ///
@@ -47,6 +63,7 @@ struct Tableau {
 enum SubCommand {
     /// Print the tableau for a given logical expression.
     Tableau(Tableau),
+    Solve(Solve),
 }
 
 #[derive(Parser, Debug)]
@@ -56,25 +73,26 @@ struct Args {
     subcommand: SubCommand,
 }
 
+fn get_source(source: Option<String>) -> String {
+    match source {
+        Some(source) => source,
+        None => {
+            let mut buffer = String::new();
+            io::stdin().read_line(&mut buffer).unwrap();
+            buffer
+        }
+    }
+}
+
 fn main() {
     let args = Args::parse();
 
     match args.subcommand {
         SubCommand::Tableau(print) => {
-            let parser = dare::Parser::new();
-
-            let source = if let Some(source) = print.source {
-                source
-            } else {
-                let mut source = String::new();
-                io::stdin().read_line(&mut source).unwrap();
-                source
-            };
+            let source = get_source(print.source);
 
             // TODO: proper error reporting
-            let expr = parser.parse(&source).unwrap();
-            let builder = TableauBuilder::default();
-            let tableau = builder.build_expression(&expr, true);
+            let tableau = dare::Tableau::parse(&source, !print.expect_false).unwrap();
 
             let output = match print.format {
                 OutputFormat::Latex => {
@@ -90,6 +108,24 @@ fn main() {
                     .unwrap()
                     .set_contents(output)
                     .unwrap();
+            }
+        }
+        SubCommand::Solve(solve) => {
+            let source = get_source(solve.source);
+
+            let tableau = dare::Tableau::parse(&source, !solve.expect_false).unwrap();
+            let mut solutions = Solutions::from(&tableau);
+            solutions.clean();
+
+            print!("Solutions:");
+
+            for solution in solutions.iter() {
+                println!();
+
+                for (variable, value) in solution.iter() {
+                    let value = if value { "T" } else { "F" };
+                    println!("\t{}: {}", variable, value);
+                }
             }
         }
     }
